4d16661f6bdbb94f119fa228736741c661025ae7
栗原恵介 <kmaronk1119@gmail.com>
2021-07-25 08:31:13 +0900

    ProductCardの作成

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,7 +1,70 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { useDispatch } from "react-redux";
+import { push } from "connected-react-router";
+import NoImage from "../../assets/img/src/no-profile.png";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    [theme.breakpoints.down("sm")]: {
+      margin: 8,
+      width: "calc(50% - 16px)",
+    },
+    [theme.breakpoints.up("sm")]: {
+      margin: 16,
+      width: "calc(33.33333% - 32px)",
+    },
+    maxWidth: 345,
+  },
+  content: {
+    display: "flex",
+    padding: "16px 8px",
+    textAlign: "left",
+    "&:last-child": {
+      paddingBottom: 16,
+    },
+  },
+  media: {
+    height: 0,
+    paddingTop: "100%",
+  },
+  price: {
+    color: theme.palette.secondary.main,
+    fontSize: 16,
+  },
+}));
 
 const ProductCard = (props) => {
-  return <div>商品情報</div>;
+  const classes = useStyles();
+  const price = props.price.toLocaleString();
+  const images = props.images.length > 0 ? props.images : [{ path: NoImage }];
+
+  const dispatch = useDispatch();
+
+  return (
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.media}
+        image={images[0].path}
+        title=""
+        onClick={() => dispatch(push("/product/" + props.id))}
+      />
+      <CardContent className={classes.content}>
+        <div onClick={() => dispatch(push("/product/" + props.id))}>
+          <Typography color="textSecondary" component="p">
+            {props.name}
+          </Typography>
+          <Typography className={classes.price} component="p">
+            ¥{price}
+          </Typography>
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default ProductCard;
