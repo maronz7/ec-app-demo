7fbf745e4917ba7a6e073e62bdf59f6ace531f02
栗原恵介 <kmaronk1119@gmail.com>
2021-07-30 09:59:33 +0900

    商品詳細ページを開く

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router";
 import * as History from "history";
+import { MuiThemeProvider } from "@material-ui/core";
+import { theme } from "./assets/theme";
 import createStore from "./reducks/store/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -13,7 +15,9 @@ export const store = createStore(history);
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <App />
+      <MuiThemeProvider theme={theme}>
+        <App />
+      </MuiThemeProvider>
     </ConnectedRouter>
   </Provider>,
   document.getElementById("root")
