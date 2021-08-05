497f9da3475a73f636704a3a36892ca86845335c
栗原恵介 <kmaronk1119@gmail.com>
2021-08-05 09:05:03 +0900

    注文の確認ページ追加

--- src/templates/index.js
+++ src/templates/index.js
@@ -6,3 +6,4 @@ export { default as Reset } from "./Reset";
 export { default as ProductEdit } from "./ProductEdit";
 export { default as ProductList } from "./ProductList";
 export { default as ProductDetail } from "./ProductDetail";
+export { default as OrderConfirm } from "./OrderConfirm";
